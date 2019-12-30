077eae6f580b841baebebe982f2e8fef9a6e3057
Piyush Kumar <piyushkumar7668@gmail.com>
2019-12-30 14:44:28 +0530

    Create Starts_with_B.py

---
+++ Dictionaries/Starts_with_B.py
@@ -0,0 +1,12 @@
+starts_with_B = {
+                  'B&':'Banned','B2B':'Business-to-business','B2C':'Business-to-consumer','B2W':'Back to work','B8':'Bait',('B/F','BF'):'Boyfriend',
+    ('B/G','BG'):'Background','B4':'Before','B4N':'Bye for now','BAE':'Babe','BAFO':'Best and final offer','BAO':'Be aware of',
+    'BASOR':'Breathing a sigh of relief','BAU':'Business as usual','BBIAB':'Be back in a bit','BBIAF':'Be back in a few',
+    'BBIAM':'Be back in a minute','BBIAS':'Be back in a sec','BBL':'Be back later','BBS':'Be back soon','BBT':'Be back tomorrow','BCZ':'Because',
+    'B/C':'Because','BCOS':'Because','BCO':'Big crush on','BCOY':'Big crush on you','BDAY':'Birthday','B-DAY':'Birthday','BFF':'Best friends forever',
+    'BFFL':'Best friends for life','BFFLNMW':'Best friends for life, no matter what','BFFN':'Best friend for now','BGWM':'Be gentle with me',
+    'BHL8':'Be home late','BIF':'Before I forget','BIH':'Burn in hell','BIL':'Brother in law','BITMT':'But in the meantime','BLNT':'etter luck next time',
+    'BME':'Based on my experience','BM&Y':'Between me and you','BRB':'Be right back','BTW':'By the way','BWL':'Bursting with laughter','Bri8':'Bright',
+    'BSDK':'Mother Fucker', 
+                  
+                  }
